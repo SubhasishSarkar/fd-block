d8f335e798c140e30f972e3e0d19d3c9a214358b
SubhasishSarkar <sarkarsubhasish207@gmail.com>
2023-11-26 23:47:41 +0530

    ev

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-   apiKey: "AIzaSyDNFOufpxVnxXMLFZbe0xvuaADaaMK9whM",
-  authDomain: "fdblock-dev.firebaseapp.com",
-  projectId: "fdblock-dev",
-  storageBucket: "fdblock-dev.appspot.com",
-  messagingSenderId: "175690632496",
-  appId: "1:175690632496:web:6c556793a4a83b4223ccf5",
-  measurementId: "G-Z65Q4PH3H7"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MESERMENT_ID,
 };
 
 // Initialize Firebase
@@ -20,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 
-export default app
+export default app;
